b079063f05f4d86fe8b2a756cf1c3dd19ae149f7
Thea <1139280884@qq.com>
2015-12-30 09:00:35 +0800

    second widget: BannerView

--- app/src/main/java/com/thea/widgets/MainActivity.java
+++ app/src/main/java/com/thea/widgets/MainActivity.java
@@ -1,25 +1,49 @@
 package com.thea.widgets;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
-import android.thea.widgets.BottomTabLayout;
+import android.thea.widgets.BannerView;
+import android.thea.widgets.ViewPagerAdapter;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomTabLayout btl;
+//    private static final String TAG = MainActivity.class.getSimpleName();
+    private BannerView bv;
+//    private ViewPager bv;
+    private ArrayList<View> mItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btl = (BottomTabLayout) findViewById(R.id.btl);
-//        btl.addTab(btl.newTab().setIcon(R.mipmap.ic_recommend).setText("aaa"));
-        btl.addTab(btl.newTab().setText("bbb"));
-        btl.addTab(btl.newTab().setText("ccc"));
-        btl.addTab(btl.newTab().setText("ddd"));
+        bv = (BannerView) findViewById(R.id.bv);
+        for (int i = 0; i < 3; i++) {
+            mItems.add(getLayoutInflater().inflate(R.layout.pager_layout, null));
+        }
+        bv.setAdapter(mViewAdapter);
+//        bv.setCurrentItem(1);
+
+//        bv.setAdapter(new MyAdapter(mItems));
+
+//        LinearLayout rl = (LinearLayout) findViewById(R.id.rl);
+//        View v = findViewById(R.id.ll_btns);
+//        CloneView cloneView = new CloneView(this, v);
+//        cloneView.setBackgroundColor(Color.RED);
+//        ViewCompat.postInvalidateOnAnimation(v);
+//        ViewCompat.postInvalidateOnAnimation(cloneView);
+//        rl.addView(cloneView);
+
     }
 
     @Override
@@ -40,5 +64,60 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }*/
         return super.onKeyDown(keyCode, event);
+    }
+
+    private ViewPagerAdapter mViewAdapter = new ViewPagerAdapter(mItems) {
+        @Override
+        public View instantiateViewItem(View view, int position) {
+//            Button button = (Button) view.findViewById(R.id.btn);
+//            button.setText("按钮" + position);
+            return view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "第" + position + "页";
+        }
+    };
+
+    public class MyAdapter extends PagerAdapter {
+        private List<View> items = new ArrayList<>();
+
+        public MyAdapter(List<View> items) {
+            this.items = items;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Log.i("MyAdapter", position + "");
+            View view = items.get(position);
+            Button button = (Button) view.findViewById(R.id.btn);
+            button.setText("按钮" + position);
+            if (container.indexOfChild(view) == -1)
+                container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = items.get(position);
+            if (container.indexOfChild(view) != -1)
+                container.removeView(view);
+        }
+
+        @Override
+        public int getCount() {
+            return items == null ? 0 : items.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "第" + position + "页";
+        }
     }
 }
